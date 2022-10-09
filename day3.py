7e2f1659b7a6a57de9ab7a830eed2ab3a2a9f22b
fouadabiad <fma48@mail.aub.edu>
2022-10-09 16:56:06 +0200

    Update day3.py

--- day3.py
+++ day3.py
@@ -3,17 +3,26 @@
 @author: Fouad
 """
 
-import numpy as np
-
-
 command = []
-
 with open('day2_data.txt') as l:
     for lines in l.readlines():
-        command[lines] = lines
+        command.append(lines[:-1]) #removes last character: \n
         
-print(command)
+up     = 0
+down   = 0
+forward= 0
 
+for i,val in enumerate(command):
+    if val[0] == 'f':
+        forward += int(val[-1])
+    elif val[0]=='u':
+        up += int(val[-1])
+    elif val[0]=='d':
+        down += int(val[-1])
+
+vertical = down - up
+print(vertical, forward)
+print(vertical * forward)
 
 
 
